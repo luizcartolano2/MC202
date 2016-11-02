31268509a53e797271a71318fd0ef9c89b0af6cf
luizcartolano2 <duducartolano@gmail.com>
2016-11-01 23:19:47 -0200

    Main is ready

--- Lab_11/main.c
+++ Lab_11/main.c
@@ -1,1 +1,26 @@
 #include "lab.h"
+
+int int main(int argc, char const *argv[]){
+
+  char command;
+  int key, cost;
+
+  do{
+    scanf("%c",&command);
+    switch (command){
+      case 'i':
+        scanf("%d %d",&key,&cost);
+        break;
+      case 'm':
+        break;
+      case 'd':
+        scanf("%d %d",&key,&cost);
+        break;
+      case 't':
+        break;
+    }
+  }while (command != 't');
+
+  return 0;
+
+}
