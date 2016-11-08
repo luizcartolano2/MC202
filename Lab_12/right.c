0ec77da0edb00e04b06b53c1ad425517b41842df
luizcartolano2 <duducartolano@gmail.com>
2016-11-08 12:36:51 -0200

    trying to implement a csr matrix

--- Lab_12/right.c
+++ Lab_12/right.c
@@ -7,32 +7,27 @@ typedef struct data{
   int value;
 }data;
 
-typedef struct row{
-  int collum_ind;
-  int valor;
-}row;
-
-typedef struct line{
-  int line_ind;
-  row *p_row;
-}line;
-
 int main(int argc, char const *argv[]) {
-  int k, i;
+  int k, i, line_m = 0;
+  int *A, *R, *C;
   data *entradas;
-  row *Row;
-  line *Lines;
 
   scanf("%d",&k);
   entradas = malloc(k * sizeof(data));
   if(!entradas)
     printf("memória insuficiente\n");
-  Row = malloc(k * sizeof(row));
-  if(!Row)
-    printf("memória insuficiente\n");  
 
-  for(i = 0; i < k; i++)
+  for(i = 0; i < k; i++){
     scanf("%d %d %d",&(entradas[i]).line,&(entradas[i]).collum,&(entradas[i]).value);
+    if(line_m < entradas[i].line)
+      line_m = entradas[i].line;
+  }
+
+  A = malloc(k * sizeof(int));
+  R = malloc((line_m+1) * sizeof(int));
+  C = malloc(k * sizeof(int));
+  if((!A) || (!R) || (!C))
+    printf("memória insuficiente\n");
 
   return 0;
 
